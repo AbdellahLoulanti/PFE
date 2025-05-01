f46d01a7886faf9285c67f1d32a9bb041b445637
AL UOLANTI ABDELLAH <156177301+AbdellahLoulanti@users.noreply.github.com>
2025-05-01 14:54:21 +0200

    u

---
+++ app/Models/Event.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'location',
+        'start_date',
+        'end_date',
+        'visibility',
+        'cover_image',
+        'tags',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'tags' => 'array',
+        'start_date' => 'datetime',
+        'end_date' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
