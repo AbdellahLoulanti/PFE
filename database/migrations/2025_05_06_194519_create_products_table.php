689880eb1e429fd16e244dcc5b0cbaaaa0e13a75
houda mazroua <mazrouahuda@gmail.com>
2025-05-06 22:58:59 +0200

    Product after review

--- database/migrations/2025_05_06_194519_create_products_table.php
+++ database/migrations/2025_05_06_194519_create_products_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->decimal('price', 8, 2);
             $table->string('image')->nullable();
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('category')->nullable();
             $table->integer('stock')->default(0);
         });
